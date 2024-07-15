bbe12a8097867c6f90f8e6311d7a7452a1725e76
Mahesh Jangam <157522470+UmaMahesh-Jangam@users.noreply.github.com>
2024-07-15 21:55:39 +0530

    Add files via upload

---
+++ Password Generated.py
@@ -0,0 +1,15 @@
+import random
+import string
+def generate_password (length):
+  characters=string.ascii_lowercase+ string.digits+string.ascii_uppercase
+  password=''.join(random.choice(characters) for i in range (length))
+  return password
+try:
+  ch = 1
+  while (ch!=0):
+     password_length=int (input("enter the desired password length:"))
+     if password_length<=0:
+       print("please enter a positive length.")
+     print("Generated password:",generate_password(password_length))    
+except ValueError:
+  print ("invalid input.please enter a valid positive integer for the password length.")
